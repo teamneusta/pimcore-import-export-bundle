73340c72311a2b6aa86b7c09a4fd09be78c5ca28
Michael Albrecht <m.albrecht@neusta.de>
2025-07-22 15:12:05 +0200

    [With or Without IDs][Export] try to fix integration tests by loading PimcoreApplicationLoggerBundle

--- tests/app/TestKernel.php
+++ tests/app/TestKernel.php
@@ -2,12 +2,14 @@
 
 use Neusta\Pimcore\ImportExportBundle\NeustaPimcoreImportExportBundle;
 use Neusta\Pimcore\TestingFramework\Kernel\TestKernel as TestingFrameworkTestKernel;
+use Pimcore\Bundle\ApplicationLoggerBundle\PimcoreApplicationLoggerBundle;
 use Pimcore\HttpKernel\BundleCollection\BundleCollection;
 
 class TestKernel extends TestingFrameworkTestKernel
 {
     public function registerBundlesToCollection(BundleCollection $collection): void
     {
+        $collection->addBundle(new PimcoreApplicationLoggerBundle());
         $collection->addBundle(new NeustaPimcoreImportExportBundle());
     }
 }
