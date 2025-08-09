d940800e83fce3a15f4bc8dd0db7314b12013f07
Michael Albrecht <m.albrecht@neusta.de>
2025-08-09 08:58:28 +0200

    [PAgeSnippet Import Error] fix

--- src/DependencyInjection/CompilerPass/RegisterTaggedConverterPass.php
+++ src/DependencyInjection/CompilerPass/RegisterTaggedConverterPass.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Neusta\Pimcore\ImportExportBundle\DependencyInjection\CompilerPass;
 
+use Neusta\ConverterBundle\TargetFactory;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\Container;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class RegisterTaggedConverterPass implements CompilerPassInterface
@@ -25,12 +27,28 @@ class RegisterTaggedConverterPass implements CompilerPassInterface
             }
             $definition = $container->getDefinition($serviceId);
 
-            if (\array_key_exists('target', $arguments)) {
-                if (!\is_string($arguments['target'])) {
-                    continue;
-                }
-                $definition->addTag('neusta.import_export.converter', ['type' => $arguments['target']]);
+            $targetType = $this->determineTargetType($container, $arguments);
+            if ($targetType) {
+                $definition->addTag('neusta.import_export.converter', ['type' => $targetType]);
             }
         }
+    }
+
+    /**
+     * @param array<string, string> $arguments
+     */
+    private function determineTargetType(Container $container, array $arguments): ?string
+    {
+        if (\array_key_exists('target', $arguments) && \is_string($arguments['target'])) {
+            return $arguments['target'];
+        }
+        if (\array_key_exists('target_factory', $arguments) && \is_string($arguments['target_factory'])) {
+            $factory = $container->get($arguments['target_factory']);
+            if ($factory instanceof TargetFactory) {
+                return \get_class($factory->create());
+            }
+        }
+
+        return null;
     }
 }
